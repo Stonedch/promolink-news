6d6f888d0f103604de1b4d7ce65b1ccd13733348
Keshah239 <keshah239@gmail.com>
2022-12-17 19:28:31 +0300

    feat: add front-end

--- app/resources/views/post/detail.blade.php
+++ app/resources/views/post/detail.blade.php
@@ -5,10 +5,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Promolink News - {{ $post->title }}</title>
 </head>
 
 <body>
+<header class="main-header">
+        <a href="#" class="logo-link" target="_self">Promolink News</a>
+        <a href="#" class="authorization"> Авторизация</a>
+    </header>
     <h2><a href="/news/{{$post->id}}">{{$post->title}}</a></h2>
     <p>{{$post->body}}</p>
 </body>
--- app/resources/views/post/list.blade.php
+++ app/resources/views/post/list.blade.php
@@ -5,24 +5,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title>Promolink News - List</title>
 </head>
 
 <body>
+    <main>
+        <header class="main-header">
+            <a href="/" class="logo-link" target="_self">Promolink News</a>
+            <a href="/platform/login/" class="authorization">Авторизация</a>
+        </header>
 
-    @foreach ($categories as $category)
-        @if (count($category->posts))
-
-            <h2>{{$category->name}}</h2>
-
-            @foreach ($category->posts as $post)
-                <h3><a href="/news/{{ $post->id }}">{{ $post->title }}</a></h3>
-                <p>{{ $post->body }}</p>
+        <div class="link-section">
+            @foreach ($categories as $category)
+                @if (count($category->posts))
+                    <section class="link-section bbc-section" data-order="0">
+                        <h2 class="col-header">{{$category->name}}</h2>
+                        <ol class="links-list links-list--bbc">
+                            @foreach ($category->posts as $post)
+                                <li>
+                                    <a href="/news/{{ $post->id }}" class="story-title">
+                                        {{ $post->title }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ol>
+                    </section>
+                @endif
             @endforeach
+        </div>
 
-        @endif
-    @endforeach
-
+        <footer class="page-footer">
+            <a href="/" class="logo-link">Promolink News</a>
+        </footer>
+    </main>
 </body>
 
 </html>
