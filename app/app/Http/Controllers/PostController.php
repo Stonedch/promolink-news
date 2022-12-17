f4b0dafcfdbf4f091cc7404fda2a54e6b113fd9b
Stonedch <stch.max.muhin@gmail.com>
2022-12-17 15:51:14 +0300

    feat: add post controller

---
+++ app/app/Http/Controllers/PostController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('post.list', [
+            'posts' => Post::all(),
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('post.detail', [
+            'post' => Post::findOrFail($id),
+        ]);
+    }
+}
