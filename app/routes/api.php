1c4329aaaa805f357306cead2194b6728e7e4292
stonedch <stch.max.muhin@gmail.com>
2022-12-26 14:10:14 +0300

    chore: update api points

--- app/routes/api.php
+++ app/routes/api.php
@@ -20,6 +20,6 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('category', CategoryController::class);
+Route::resource('categories', CategoryController::class);
 
-Route::resource('post', PostController::class);
+Route::resource('posts', PostController::class);
